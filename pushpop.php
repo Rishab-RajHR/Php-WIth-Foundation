b642665c491df79826d4e54a277ee5a2339159c0
Rishab Raj <rr0948555@gmail.com>
2026-06-24 16:52:28 +0530

    PushPop

---
+++ pushpop.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Push Pop Method</title>
+</head>
+<body>
+<?php 
+$a = array('C', 'C++', 'Java');
+
+echo "<br> Original Array is <br>";
+
+print_r($a);
+
+?>
+</body>
+</html>
