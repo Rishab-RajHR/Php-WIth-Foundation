a9b8953468ed9f24f3a6728a685e649f5a9e88cd
Rishab Raj <rr0948555@gmail.com>
2026-06-27 14:58:05 +0530

    DateFunction

---
+++ DateFunction.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Date Function</title>
+ 
+</head>
+<body>
+<?php 
+echo date('d-F-y z g:i:s a');
+/*
+d   -date of month
+m   -month in digits
+M   -month in short form alphabets
+F   -month in full form alphabets
+y   -year in two digits
+Y   -year in four digits
+h   -time in two digits 12 hours
+H   -time in two digits 24 hours
+g   -time required(1 or 2)digits 12 hours
+G   -time required(1 or 2)digits 24 hours
+i   -minutes
+s   -seconds
+a   -am/pm
+z   -day month
+S   -suffix
+*/
+
+?>
+</body>
+</html>
