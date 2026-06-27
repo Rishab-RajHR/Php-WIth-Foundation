d9a4499463f115a7a2a354dca56c00583bc3fc7c
Rishab Raj <rr0948555@gmail.com>
2026-06-27 14:22:46 +0530

    Explode Checkout

--- explodecheckout.php
+++ explodecheckout.php
@@ -18,7 +18,13 @@ $y = $a[2];
 
 if(checkdate($m,$d,$y))
 //checkdate function is used to validate date
-
+{
+   echo "<br> A Valid Date";
+}
+else
+{
+    echo "<br> A Invalid Date";
+}
 ?>
 </body>
 </html>
