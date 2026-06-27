53d21c0b3ef3b2e65dd17c53aa674ff66e43abfe
Rishab Raj <rr0948555@gmail.com>
2026-06-27 14:18:01 +0530

    Explode Checkout

---
+++ explodecheckout.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title> Explode and Checkout in PHP</title>
+ 
+</head>
+<body>
+<?php 
+$date = "31-10-2025";
+$a = explode("-",$date);
+// explode function break a string into array
+
+$d = $a[0];
+$m = $a[1];
+$y = $a[2];
+
+if(checkdate($m,$d,$y))
+//checkdate function is used to validate date
+
+?>
+</body>
+</html>
