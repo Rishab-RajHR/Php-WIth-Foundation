ffd0823aadf26c6624052e2ca48ed8e76d6bef7c
Rishab Raj <rr0948555@gmail.com>
2026-07-01 16:56:19 +0530

    Graphics

---
+++ Graphics/lines.php
@@ -0,0 +1,12 @@
+<?php
+$im = imagecreate(200, 200) or die("Cannot Initialize new GD image stream");
+
+imagecolorallocate($im,255,255,0);
+$red = imagecolorallocate($im,255,0,0); // red
+$blue = imagecolorallocate($im,0,0,255); // blue
+imageline($im, 5, 5, 195, 5, $red);
+imagepng($im, "image1.png");
+imagedestroy($im);
+
+echo "<img src = image2.png>";
+?>
