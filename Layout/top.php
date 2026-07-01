2335ab1a08bb87806e2c386af84c730a9e6c191c
Rishab Raj <rr0948555@gmail.com>
2026-07-01 17:29:56 +0530

    layout

---
+++ Layout/top.php
@@ -0,0 +1,11 @@
+<html>
+  <head>
+
+  <body>
+      <div style="border : 1px solid red; height: 85px; text-align: center; color: red; font-size: 30px; background-color: pink; font-family: algerian;">
+          YCT Academy
+      </div>
+  </body>
+  
+  </head>
+</html>
