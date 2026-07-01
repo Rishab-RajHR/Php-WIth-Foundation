b17cd5e3ed67a72c89fa3ba28d2db636cb1b7b34
Rishab Raj <rr0948555@gmail.com>
2026-07-01 16:57:51 +0530

    Graphics

--- Graphics/lines.php
+++ Graphics/lines.php
@@ -5,6 +5,7 @@ imagecolorallocate($im,255,255,0);
 $red = imagecolorallocate($im,255,0,0); // red
 $blue = imagecolorallocate($im,0,0,255); // blue
 imageline($im, 5, 5, 195, 5, $red);
+imageline($im, 5, 5, 195, 5, $blue);
 imagepng($im, "image1.png");
 imagedestroy($im);
 
