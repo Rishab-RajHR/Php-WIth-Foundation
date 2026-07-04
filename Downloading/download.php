6d37d6817e3efe6ce8f6c7cbcb5b1484932f70c8
Rishab Raj <rr0948555@gmail.com>
2026-07-04 11:09:40 +0530

    Download

---
+++ Downloading/download.php
@@ -0,0 +1,10 @@
+<html>
+  <head>
+       <title>Downloading Section</title>
+  </head>
+  <body>
+      <a href="downloads/Certificate.pdf">Download Certificate</a><br>
+      <a href="downloads/Rishab_Internship.Report2_removed.pdf">Download Internship Report</a>
+      <a href="downloads/Rishab_Internship.Report2_removed.pdf" download="">Download Internship Report</a>
+  </body>
+</html>
