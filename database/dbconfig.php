cf52e8d0830df64c24616b0546125dd3413a8f5e
Rishab Raj <rr0948555@gmail.com>
2026-07-05 13:55:34 +0530

    Database

--- database/dbconfig.php
+++ database/dbconfig.php
@@ -9,10 +9,20 @@ function my_iud($query) // insert, update, delete
   global $dbserver, $dbuser, $dbpwd, $dbname;
 $cid = mysqli_connect($dbserver, $dbuser, $dbpwd, $dbname) or die("Connection Failed");
 
-mysqli_select_db($dbname, $cid);
-mysqli_query($query, $cid);
+mysqli_select_db($cid, $dbname);
+mysqli_query($cid, $query);
 $n = mysqli_affected_rows($cid);
 return $n;
+}
+
+function my_select($query) // search
+{
+  global $dbserver, $dbuser, $dbpwd, $dbname;
+$cid = mysqli_connect($dbserver, $dbuser, $dbpwd, $dbname) or die("Connection Failed");
+
+mysqli_select_db($cid, $dbname);
+$rs = mysqli_query( $cid, $query);
+return $rs;
 }
 
 ?>
