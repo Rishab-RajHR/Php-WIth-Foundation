5d1b0572e35df35396dadc12b348e6dbf7b27aa2
Rishab Raj <rr0948555@gmail.com>
2026-07-08 12:26:29 +0530

    Adding the Bottom Part

---
+++ Layout/bottom.php
@@ -0,0 +1,7 @@
+<html>
+    <body>
+         <div style="border: 1px solid red; background-color: black; color: white; text-align: center; position: absolute; top: 95%; width: 100%;">
+             Copyright &copy; YCT Academy , Designed By Alex Pandian
+         </div>
+    </body>
+</html>
