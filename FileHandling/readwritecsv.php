c0b75a1dc15f88e1a6ccc74e9e77156a1b02c0ff
Rishab Raj <rr0948555@gmail.com>
2026-07-15 13:09:11 +0530

    Read Write

---
+++ FileHandling/readwritecsv.php
@@ -0,0 +1,25 @@
+<html>
+    <head>
+        <title>Handling CSV Files</title>
+    </head>
+    <body>
+        <h2 align="center">Enter Student Data</h2>
+        <form method="post">
+            Enter Roll Number <input type="text" name="rollno">
+            <br>
+            Enter Name <input type="text" name="sname">
+            <br>
+            Enter Semester <input type="text" name="sem">
+            <br>
+            Enter Branch <input type="text" name="branch">
+            <br>
+            Enter Marks <input type="text" name="rollno">
+            <br>
+            Enter Roll Number <input type="text" name="rollno">
+            <br>
+            <input type="submit" name="save" value="Save">
+            <input type="submit" name="list" value="list">
+            <input type="submit" name="remove" value="remove">
+        </form>
+    </body>
+</html>
